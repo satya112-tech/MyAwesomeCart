6ffc17f35a5b997a638276d1f02b7605a4dbeb4a
Satya <satyajeetm.2021@gmail.com>
2023-11-27 23:48:12 +0530

    first commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("",views.index,name="BlogHome"),
+    path('', views.index,name="blogHome"),
+    path('blogpost/<int:id>', views.blogpost,name="blogHome"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,12 @@ from django.http import HttpResponse
 from . models import Product,Contact,Orders,OrderUpdate
 from math import ceil
 import json
+from django.views.decorators.csrf import csrf_exempt
+from PayTm import Checksum
+
+MERCHANT_KEY = 'Your-Merchant-Key-Here' + ' ' * (32 - len('Your-Merchant-Key-Here'))
+
+
 
 def index(request):
     products= Product.objects.all()
@@ -33,12 +39,13 @@ def tracker(request):
                 updates=[]
                 for item in update:
                     updates.append({'text':item.update_desc,'time':item.timestamp})
-                    response = json.dumps([updates, order[0].items_json],default=str)
+                    # json.dumps() convert dictionary into json
+                    response = json.dumps({"status":"success", "updates": updates, "itemsJson": order[0].items_json}, default=str)
                 return HttpResponse(response)
             else:
-                return HttpResponse('{}')
+                return HttpResponse('{"status":"noitem"}')
         except Exception as e:
-            return HttpResponse('{}')
+            return HttpResponse('{"status":"error"}')
     return render(request,'shop/tracker.html')
 
 
@@ -54,9 +61,31 @@ def contact(request):
         thank=True
     return render(request,'shop/contact.html',{'thank':thank})
 
-
+def searchMatch(query, item):
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+    
+    
 def search(request):
-    return render(request,'shop/search.html')
+    query= request.GET.get('search')
+    allprods=[]
+    catprods=Product.objects.values('category','id')
+    cats={item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod=[item for item in prodtemp if searchMatch(query, item)]
+        n = len(prod)
+        nslides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod)!= 0:
+            allprods.append([prod, range(1, nslides), nslides])
+    params = {'allprods': allprods, "msg":""}
+    if len(allprods)==0 or len(query)<4:
+        params={'msg':"Please make sure to enter relevant search query"}
+    return render(request,'shop/search.html',params)
+
+
 def productview(request,myid):
     product=Product.objects.filter(id=myid)
     return render(request,'shop/productview.html',{'product':product[0]})
@@ -65,18 +94,64 @@ def checkout(request):
     if request.method=="POST":
         items_json = request.POST.get('itemsJson', '')
         name = request.POST.get('name', '')
+        amount = request.POST.get('amount', '')
         email = request.POST.get('email', '')
         address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
         city = request.POST.get('city', '')
         state = request.POST.get('state', '')
         zip_code = request.POST.get('zip_code', '')
         phone = request.POST.get('phone', '')
-        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,state=state, zip_code=zip_code, phone=phone)
+        order = Orders(items_json=items_json, name=name, amount=amount, email=email, address=address, city=city,state=state, zip_code=zip_code, phone=phone)
         order.save()
         #58,59 line for OrderUpdate
         update = OrderUpdate(order_id=order.order_id, update_desc="The order has been placed")
         update.save()
         thank=True
         id=order.order_id
-        return render(request,'shop/checkout.html',{'thank':thank,'id':id})
+        #return render(request,'shop/checkout.html',{'thank':thank,'id':id})
+        #Request paytm to transfer the abount to account after payment by user
+        param_dict={
+            #merchant id
+            'MID': 'Your-Merchant-Id-Here',
+            'ORDER_ID': str(order.order_id),
+            'TXN_AMOUNT': str(amount),
+            'CUST_ID': email,
+            'INDUSTRY_TYPE_ID': 'Retail',
+            'WEBSITE': 'WEBSTAGING',
+            'CHANNEL_ID': 'WEB',
+            #Callback URL: It is the URL where you want Paytm to show you the payment status.
+            'CALLBACK_URL':'http://127.0.0.1:8000/shop/handlerequest/',
+
+        }
+        param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict, MERCHANT_KEY)
+        return  render(request, 'shop/paytm.html', {'param_dict': param_dict})
     return render(request, 'shop/checkout.html')
+
+@csrf_exempt
+def handlerequest(request):
+    # Ensure checksum is defined with a default value
+    checksum = None
+
+    # paytm will send you post request here
+    form = request.POST
+    response_dict = {}
+
+    for i in form.keys():
+        response_dict[i] = form[i]
+        if i == 'CHECKSUMHASH':
+            checksum = form[i]
+
+    # Check if 'CHECKSUMHASH' is found in the form
+    if checksum is not None:
+        # Verify checksum after the loop
+        verify = Checksum.verify_checksum(response_dict, MERCHANT_KEY, checksum)
+
+        if verify:
+            if response_dict['RESPCODE'] == '01':
+                print('Order successful')
+            else:
+                print('Order was not successful because ' + response_dict['RESPMSG'])
+    else:
+        print('CHECKSUMHASH not found in the form')
+
+    return render(request, 'shop/paymentstatus.html', {'response': response_dict})
